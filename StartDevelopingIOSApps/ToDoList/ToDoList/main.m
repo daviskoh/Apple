67aa69198178a2749fc4275bcf67cdb315131540
daviskoh <koh.davis.0@gmail.com>
2015-03-15 22:08:24 -0400

    add main.m file notes

--- StartDevelopingIOSApps/ToDoList/ToDoList/main.m
+++ StartDevelopingIOSApps/ToDoList/ToDoList/main.m
@@ -11,6 +11,16 @@
 
 int main(int argc, char * argv[]) {
   @autoreleasepool {
+      /**
+       * UIApplicationMain creates 2 components:
+       * 
+       * 1) instance of UIApplication (application object)
+       * - manages app event loop & other behaviors
+       * 
+       * 2) instance of AppDelegate (app delegate)
+       * - creates window where content is drawn
+       * - provides place to resp to state transitions
+       */
       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
