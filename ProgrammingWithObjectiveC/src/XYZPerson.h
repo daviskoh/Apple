c8a9b632a09fbf22fa6e194cecdadd246e12148a
Davis Koh <koh.davis.0@gmail.com>
2015-03-16 17:24:06 -0400

    add comments on instance vars

--- ProgrammingWithObjectiveC/src/XYZPerson.h
+++ ProgrammingWithObjectiveC/src/XYZPerson.h
@@ -2,10 +2,16 @@
 
 @interface XYZPerson: NSObject
 
+// readwrite property is backed by instance variable synthesized by default
+// instance var - var that exists & holds value for life of object
+// - memory used for instance var allocated when obj created 
+// - memory used for instance var freed when obj deallocated
+// - vars synthesized by compiler w/ '_' prefix
 @property (readwrite, assign) NSString *firstName;
 @property (readwrite, assign) NSString *lastName;
 @property (readonly) NSString *fullName;
 @property (readonly) NSDate *dateOfBirth;
+
 // can add multi attrs to properties via comma separation
 // can specify custom getter as is customary for boolean properties
 @property (readonly, getter=isFinished) BOOL finished;
--- ProgrammingWithObjectiveC/src/XYZPerson.m
+++ ProgrammingWithObjectiveC/src/XYZPerson.m
@@ -2,6 +2,11 @@
 
 @implementation XYZPerson
 
+// can synthesize custom names for instance var ex:
+// @synthesize firstName = ivar_firstName;
+// - will allow prop to still be accessible via firstName, etc
+// - now will be backed by instance var called ivar_firstName
+
 + (id)person {
     return [[self alloc] init];
 }
