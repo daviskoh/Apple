adc8b669e676690e5ecd1bc9835be7e9f68b32a9
Davis Koh <koh.davis.0@gmail.com>
2015-04-03 18:22:59 -0400

    add note on blocks

--- ProgrammingWithObjectiveC/src/XYZSimpleBlock.m
+++ ProgrammingWithObjectiveC/src/XYZSimpleBlock.m
@@ -1,3 +1,7 @@
 // best practice to define custom type when specific block signaure is reused
+// - also makes code more readable when used in method defs where
+// blocks return other blocks, etc
+
+// NOTE: blocks are 1st class citizens and can be passed around & returned
 typedef void (^XYZSimpleBlock)(void);
 
