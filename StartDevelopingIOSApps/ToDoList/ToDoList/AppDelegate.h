5b02ef28e06242848257034bc38abe9a557ccdd0
daviskoh <koh.davis.0@gmail.com>
2015-04-15 22:21:10 -0400

    add notes on view hierarchy

--- StartDevelopingIOSApps/ToDoList/ToDoList/AppDelegate.h
+++ StartDevelopingIOSApps/ToDoList/ToDoList/AppDelegate.h
@@ -10,8 +10,17 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-// used to keep track of window where ALL content is drawn
+// UIWindow instance used to keep track of window where ALL content is drawn
 @property (strong, nonatomic) UIWindow *window;
+
+/**
+ * view hiearchy - layout of views relative to others in hierarchical structure
+ * - instance of UIWindow is at top of hierarchy
+ *
+ * For view to be VISIBLE to user:
+ * content view w/ its subviews need to be inserted into window's hierarchy
+ *
+ */
 
 @end
 
