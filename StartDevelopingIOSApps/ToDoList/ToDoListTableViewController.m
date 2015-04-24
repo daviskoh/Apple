9ab0cabdabb3eebd9d5b2e06d7ae71ed874f83bb
Davis Koh <koh.davis.0@gmail.com>
2015-04-24 18:32:45 -0400

    begin implementing UITableViewDataSource protocol methods

--- StartDevelopingIOSApps/ToDoList/ToDoListTableViewController.m
+++ StartDevelopingIOSApps/ToDoList/ToDoListTableViewController.m
@@ -60,15 +60,14 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [_toDoItems count];
 }
 
 /*
