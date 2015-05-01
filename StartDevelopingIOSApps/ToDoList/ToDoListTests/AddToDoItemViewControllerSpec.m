411587c5d6609bed9907970a79f7095f235866b6
daviskoh <koh.davis.0@gmail.com>
2015-05-01 00:13:39 -0400

    add AddToDoItemViewControllerSpec skeleton

---
+++ StartDevelopingIOSApps/ToDoList/ToDoListTests/AddToDoItemViewControllerSpec.m
@@ -0,0 +1,39 @@
+//
+//  AddToDoItemViewControllerSpec.m
+//  ToDoList
+//
+//  Created by Davis Koh on 4/30/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "AddToDoItemViewController.h"
+
+@interface AddToDoItemViewControllerSpec : XCTestCase
+
+@end
+
+@implementation AddToDoItemViewControllerSpec
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+#pragma mark - Test Navigation
+
+- (void)testSave {
+    
+}
+
+- (void)testCancel {
+    
+}
+
+@end
